7ab7208088c3f924b31d3a8846c8fb7ae2ba0805
code-Lance <hyunjune2001@gmail.com>
2026-08-13 14:45:23 +0900

    [Feat] 프로젝트 파일 생성 1. 프로젝트 파일 생성 - 프로젝트 기본 파일 생성

---
+++ src/main/java/com/bidan/BidanApplication.java
@@ -0,0 +1,13 @@
+package com.bidan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bidan/BidanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
