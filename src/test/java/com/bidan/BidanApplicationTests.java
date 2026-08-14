170c2d8894c966050366e57dd71a94635a0ae25e
code-Lance <hyunjune2001@gmail.com>
2026-08-14 13:57:59 +0900

    [Feat] 공통 API 응답 테스트 파일 추가 1. 공통 API 응답 테스트 파일 추가

--- src/test/java/com/bidan/BidanApplicationTests.java
+++ src/test/java/com/bidan/BidanApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bidan;
+/*package com.bidan;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class BidanApplicationTests {
 	}
 
 }
+*/
