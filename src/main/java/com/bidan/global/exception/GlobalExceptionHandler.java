b5118dccd8c925799ff814e1c71edd81c129ce39
code-Lance <hyunjune2001@gmail.com>
2026-08-14 13:48:05 +0900

    [Feat] 공통 API 응답 생성 1. 공통 API 응답 생성 - 공통 API 응답 생성 - 오류 발생 시 API 응답 통일화 2. discord 관련 디렉토리 생성

--- src/main/java/com/bidan/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bidan/global/exception/GlobalExceptionHandler.java
@@ -3,21 +3,22 @@ package com.bidan.global.exception;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+import com.bidan.global.response.ApiResponse;
 /* Exception 예외 발생 시 Handler를 통해 비즈니스 예외,
 Checked Exception를 인식하여 아래 방식으로 처리
 이때 BusinessException은 unchecked Exception 관련 예외 */
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ErrorResponse> handleBusinessException(
+    public ResponseEntity<ApiResponse<Void>> handleBusinessException(
             BusinessException e
     ) {
 
         ErrorCode errorCode = e.getErrorCode();
 
-        ErrorResponse response = new ErrorResponse(
+        ApiResponse<Void> response = ApiResponse.error(
                 errorCode.getCode(),
                 errorCode.getMessage()
         );
